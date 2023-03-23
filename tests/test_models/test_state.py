40e4971a7a0a84ec470d1d90d38d0dee833ef311
Quzeem Odusanwo <108905812+Quzeemoddusanwo@users.noreply.github.com>
2023-03-23 05:52:32 +0100

    Update test_state.py

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,22 +1,40 @@
 #!/usr/bin/python3
-""" """
-from tests.test_models.test_base_model import test_basemodel
+"""Module for test State class"""
+import unittest
+import json
+import pep8
+import datetime
+
 from models.state import State
-import os
+from models.base_model import BaseModel
 
 
-class test_state(test_basemodel):
-    """ states test class"""
+class TestState(unittest.TestCase):
+    """Test State class implementation"""
+    def test_doc_module(self):
+        """Module documentation"""
+        doc = State.__doc__
+        self.assertGreater(len(doc), 1)
 
-    def __init__(self, *args, **kwargs):
-        """ state test class init"""
-        super().__init__(*args, **kwargs)
-        self.name = "State"
-        self.value = State
+    def test_pep8_conformance_state(self):
+        """Test that models/state.py conforms to PEP8."""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['models/state.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
 
-    def test_name3(self):
-        """ testing state name attr"""
-        new = self.value()
-        self.assertEqual(type(new.name), str if
-                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
-                         type(None))
+    def test_pep8_conformance_test_state(self):
+        """Test that tests/test_models/test_state.py conforms to PEP8."""
+        pep8style = pep8.StyleGuide(quiet=True)
+        res = pep8style.check_files(['tests/test_models/test_state.py'])
+        self.assertEqual(res.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
+    def test_doc_constructor(self):
+        """Constructor documentation"""
+        doc = State.__init__.__doc__
+        self.assertGreater(len(doc), 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
